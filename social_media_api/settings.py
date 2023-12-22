9baa831654cdf7353e810bb25e52d1312a007500
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-12-22 12:34:51 +0100

    Added .env with Secret key and loaded into settings.py using python_dotenv module

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 from datetime import timedelta
 from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5@oz-bgdd0pd$x1+5=7ppqhjaw3$fd$3vnifcz5=fb4cvl%$-@"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
