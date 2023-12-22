d0be8368f0c039d8ac99cfc69acd45ee8dbe624a
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-12-22 12:33:11 +0100

    Renamed constant

--- user/permission.py
+++ user/permission.py
@@ -1,11 +1,11 @@
 from rest_framework import permissions
 
-SAFE_METHODS = ("GET", "POST", "HEAD", "OPTIONS")
+SAFE_METHODS_WITH_POST = ("GET", "POST", "HEAD", "OPTIONS")
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj) -> bool:
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS_WITH_POST:
             return True
 
         return obj.user == request.user
